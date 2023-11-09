848c996d316f158f2416f6aaa69eb7d0ca3b95db
DineshDDi <70742612+DineshDDi@users.noreply.github.com>
2023-11-09 13:39:29 +0530

    Update views.py

--- testapp/views.py
+++ testapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+# Basic Python Page
 def index(request):
     return HttpResponse("<html><h1>This is CICD test page version-1.0</h1></html>")
 
