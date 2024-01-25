6e2bd400ef6760ea7a2bdf9c4674bf6c717e490c
DineshDDi <70742612+DineshDDi@users.noreply.github.com>
2024-01-25 12:10:19 +0530

    Update views.py

--- testapp/views.py
+++ testapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Basic Python Page
 def index(request):
-    return HttpResponse("<html><h1>This is CICD test page version-2.0</h1></html>")
+    return HttpResponse("<html><h1>This is CICD test page version-3.0</h1></html>")
 
 
 
